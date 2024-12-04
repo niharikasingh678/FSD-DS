15e2700374c6cc8fc36ac8bd0040af3d304aa77a
niharikasingh678 <niharika250104@gmail.com>
2024-12-04 14:16:07 +0530

    done

---
+++ Day11/react-hooks-app/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import CounterApp from './components/CounterApp';
+import ImageApp from './components/ImageApp';
+
+const App = () => {
+  return (
+    <div>
+      <ImageApp />
+    </div>
+  )
+}
+
+export default App
---
+++ Day11/react-hooks-app/src/components/ImageApp.jsx
@@ -0,0 +1,39 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import "./ImageApp.css";
+import pic from "../images/1.jpg";
+
+const ImageApp = () => {
+    const [width, setWidth] = useState(200);
+    const [height, setHeight] = useState(200);
+    const [rotation, setRotation] = useState(0);
+
+    const increaseWidth = () => setWidth(width + 20);
+    const decreaseWidth = () => setWidth(width - 20);
+    const increaseHeight = () => setHeight(height + 20);
+    const decreaseHeight = () => setHeight(height - 20);
+    const rotateImage = () => setRotation(rotation + 45);
+
+    return (
+        <div>
+            <div 
+                style={{
+                    display: "inline-block",
+                    transform: rotate($,{rotation},deg),
+                    transition: "transform 0.3s",
+                }}
+            >
+                <img src={pic} width={width} height={height} alt="Dynamic" />
+            </div>
+            <div>
+                <button onClick={increaseWidth}>Increase Width</button>
+                <button onClick={decreaseWidth}>Decrease Width</button>
+                <button onClick={increaseHeight}>Increase Height</button>
+                <button onClick={decreaseHeight}>Decrease Height</button>
+                <button onClick={rotateImage}>Rotate</button>
+            </div>
+        </div>
+    );
+};
+
+export default ImageApp;
